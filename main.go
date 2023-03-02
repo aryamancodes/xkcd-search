bed3dde9bad5360dea2be2854f56ddb4cd17d017
Aryaman Gupta <aryamangupta2002@gmail.com>
2023-03-01 23:00:00 -0500

    api: play around with fetching and parsing explanation

--- main.go
+++ main.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	// "io/ioutil"
-	// "log"
-	// "net/http"
-
-	// "github.com/tidwall/gjson"
-
+	"io/ioutil"
+	"log"
+	"net/http"
 	"xkcd/util"
+
+	"github.com/tidwall/gjson"
 )
 
 func main() {
 	util.GetAllComics()
+	resp, err := http.Get("https://www.explainxkcd.com/wiki/api.php?action=parse&page=2741:_Wish_Interpretation&format=json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	explainedHTML := gjson.Get(string(body), "parse.text")
+	log.Println(explainedHTML)
 }
-
-// resp, err := http.Get("https://www.explainxkcd.com/wiki/api.php?action=parse&page=2741:_Wish_Interpretation&format=json")
-// if err != nil {
-// 	log.Fatalln(err)
-// }
-
-// body, err := ioutil.ReadAll(resp.Body)
-// if err != nil {
-// 	log.Fatalln(err)
-// }
-
-// explainedHTML := gjson.Get(string(body), "parse.text")
-// log.Println(explainedHTML)
-//}
