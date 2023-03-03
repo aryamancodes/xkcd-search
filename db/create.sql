1d7e4195862eaa5a063224348d9867ad8593d768
Aryaman Gupta <aryamangupta2002@gmail.com>
2023-03-03 00:23:31 -0500

    database: update shema to include foreign keys

--- db/create.sql
+++ db/create.sql
@@ -1,22 +1,21 @@
-DROP TABLE IF EXISTS comic;
-
-CREATE TABLE comic (
-  num INTEGER NOT NULL UNIQUE,
+DROP TABLE IF EXISTS Comics;
+CREATE TABLE Comics (
+  id INTEGER NOT NULL UNIQUE,
   comicDay NUMERIC(2) NOT NULL,
   comicMonth NUMERIC(2) NOT NULL,
   comicYear NUMERIC(4) NOT NULL,
   title BLOB NOT NULL,
   alt BLOB NOT NULL,
-  transcript BLOB NOT NULL,
+  transcript BLOB,
   img VARCHAR(200) characterSET utf8mb4 NOT NULL,
   PRIMARY KEY (`num`)
 );
 
-DROP TABLE IF EXISTS tf_idf;
-
-CREATE TABLE tf_idf (
+DROP TABLE IF EXISTS TermFrequency;
+CREATE TABLE TermFrequency (
+  comic_id INTEGER, 
   term TEXT,
-  freq INTEGER,
-  num INTEGER,
-  PRIMARY KEY (`num`)
+  frequency INTEGER,
+  UNIQUE(comic_id, term)
+  FOREIGN KEY (`num`) REFERENCES Comics(id) 
 );
